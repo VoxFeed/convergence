87ceab17662e6b5722f21ec2132314e29c6bc9b8
Gus Ortiz <guspidey@gmail.com>
2016-07-17 09:52:42 -0500

    Supports queries in memory with boolean operators

--- test/drivers/memory/filter.spec.js
+++ test/drivers/memory/filter.spec.js
@@ -15,7 +15,7 @@ const schema = defineSchema('single_table', {
   createdAt: types.DATE
 });
 const fixtures = require('./../../data/fixtures/persons');
-const store = fixtures.map(clone);
+const store = {'single_table': fixtures.map(clone)};
 const filter = MemoryFilter(schema, store);
 
 test('should find item with one condition', (t) => {
@@ -57,6 +57,55 @@ test('should find with date ranges in one field $gt and $lte', (t) => {
   const endDate = new Date('2015-12-01T00:00:00.000Z');
   const uql = {createdAt: {$gt: new Date(startDate), $lte: new Date(endDate)}};
   const expected = [3, 5];
+  const actual = filter(uql).map(p => p.id).sort();
+  t.equal(actual.join(), expected.join());
+  t.end();
+});
+
+test('should create correct conditions with three regular conditions and a date range condition', (t) => {
+  const startDate = new Date('2015-12-01T00:00:00.000Z');
+  const endDate = new Date('2016-01-18T00:00:00.000Z');
+  const regularConds = {name: 'Jon'};
+  const dateRange = {
+    createdAt: {
+      $gte: new Date(startDate),
+      $lte: new Date(endDate)
+    }
+  };
+  const uql = Object.assign({}, regularConds, dateRange);
+  const expected = [1];
+  const actual = filter(uql).map(p => p.id).sort();
+  t.equal(actual.join(), expected.join());
+  t.end();
+});
+
+test('should find records with $or operator', (t) => {
+  const uql = {$or: [{name: 'Gibran'}, {lastName: 'Argumedo'}]};
+  const expected = [3, 4];
+  const actual = filter(uql).map(p => p.id).sort();
+  t.equal(actual.join(), expected.join());
+  t.end();
+});
+
+test('should find no records with $or operator and impossible conditions', (t) => {
+  const uql = {$or: [{name: 'Leafar'}, {lastName: 'Huichops'}]};
+  const expected = [];
+  const actual = filter(uql).map(p => p.id).sort();
+  t.equal(actual.join(), expected.join());
+  t.end();
+});
+
+test('should find records with $and operator', (t) => {
+  const uql = {$and: [{name: 'Gibran'}, {lastName: 'Arias'}]};
+  const expected = [3];
+  const actual = filter(uql).map(p => p.id).sort();
+  t.equal(actual.join(), expected.join());
+  t.end();
+});
+
+test('should find no records with $and operator and impossible conditions', (t) => {
+  const uql = {$and: [{name: 'Gibran'}, {lastName: 'Argumedo'}]};
+  const expected = [];
   const actual = filter(uql).map(p => p.id).sort();
   t.equal(actual.join(), expected.join());
   t.end();

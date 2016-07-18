9e523f61be56fdd8e7095a4a443d774ae7fd432c
Gus Ortiz <guspidey@gmail.com>
2016-07-18 10:15:49 -0500

    Use suitape in tests

--- test/drivers/memory/filter.spec.js
+++ test/drivers/memory/filter.spec.js
@@ -1,4 +1,4 @@
-const test = require('tape');
+const {suite} = require('suitape');
 const clone = require('lodash/cloneDeep');
 const get = require('lodash/get');
 const first = require('lodash/first');
@@ -18,119 +18,108 @@ const fixtures = require('./../../data/fixtures/persons');
 const store = {'single_table': fixtures.map(clone)};
 const filter = MemoryFilter(schema, store);
 
-test('should find item with one condition', (t) => {
-  const uql = {name: 'Jon'};
-  const expected = 1;
-  const actual = get(first(filter(uql)), 'id', null);
-  t.equal(actual, expected);
-  t.end();
-});
+suite('Memory Filter', (test) => {
+  test('should find item with one condition', (assert) => {
+    const uql = {name: 'Jon'};
+    const expected = 1;
+    const actual = get(first(filter(uql)), 'id', null);
+    assert('equal', actual, expected);
+  });
 
-test('should find item with two conditions', (t) => {
-  const uql = {name: 'Jon', lastName: 'Doe'};
-  const expected = 1;
-  const actual = get(first(filter(uql)), 'id', null);
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should find item with two conditions', (assert) => {
+    const uql = {name: 'Jon', lastName: 'Doe'};
+    const expected = 1;
+    const actual = get(first(filter(uql)), 'id', null);
+    assert('equal', actual, expected);
+  });
 
-test('should find with three conditions', (t) => {
-  const uql = {name: 'Jon', lastName: 'Doe', age: 23};
-  const expected = 1;
-  const actual = get(first(filter(uql)), 'id', null);
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should find with three conditions', (assert) => {
+    const uql = {name: 'Jon', lastName: 'Doe', age: 23};
+    const expected = 1;
+    const actual = get(first(filter(uql)), 'id', null);
+    assert('equal', actual, expected);
+  });
 
-test('should find with date ranges in one field $gte and $lt', (t) => {
-  const startDate = new Date('2015-09-01T00:00:00.000Z');
-  const endDate = new Date('2016-01-01T00:00:00.000Z');
-  const uql = {createdAt: {$gte: new Date(startDate), $lt: new Date(endDate)}};
-  const expected = [3, 4, 5, 6];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find with date ranges in one field $gte and $lt', (assert) => {
+    const startDate = new Date('2015-09-01T00:00:00.000Z');
+    const endDate = new Date('2016-01-01T00:00:00.000Z');
+    const uql = {createdAt: {$gte: new Date(startDate), $lt: new Date(endDate)}};
+    const expected = [3, 4, 5, 6];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find with date ranges in one field $gt and $lte', (t) => {
-  const startDate = new Date('2015-10-01T00:00:00.000Z');
-  const endDate = new Date('2015-12-01T00:00:00.000Z');
-  const uql = {createdAt: {$gt: new Date(startDate), $lte: new Date(endDate)}};
-  const expected = [3, 5, 6];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find with date ranges in one field $gt and $lte', (assert) => {
+    const startDate = new Date('2015-10-01T00:00:00.000Z');
+    const endDate = new Date('2015-12-01T00:00:00.000Z');
+    const uql = {createdAt: {$gt: new Date(startDate), $lte: new Date(endDate)}};
+    const expected = [3, 5, 6];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should create correct conditions with three regular conditions and a date range condition', (t) => {
-  const startDate = new Date('2015-12-01T00:00:00.000Z');
-  const endDate = new Date('2016-01-18T00:00:00.000Z');
-  const regularConds = {name: 'Jon'};
-  const dateRange = {
-    createdAt: {
-      $gte: new Date(startDate),
-      $lte: new Date(endDate)
-    }
-  };
-  const uql = Object.assign({}, regularConds, dateRange);
-  const expected = [1];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should create correct conditions with three regular conditions and a date range condition', (assert) => {
+    const startDate = new Date('2015-12-01T00:00:00.000Z');
+    const endDate = new Date('2016-01-18T00:00:00.000Z');
+    const regularConds = {name: 'Jon'};
+    const dateRange = {
+      createdAt: {
+        $gte: new Date(startDate),
+        $lte: new Date(endDate)
+      }
+    };
+    const uql = Object.assign({}, regularConds, dateRange);
+    const expected = [1];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find records with $or operator', (t) => {
-  const uql = {$or: [{name: 'Gibran'}, {lastName: 'Argumedo'}]};
-  const expected = [3, 4];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find records with $or operator', (assert) => {
+    const uql = {$or: [{name: 'Gibran'}, {lastName: 'Argumedo'}]};
+    const expected = [3, 4];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find no records with $or operator and impossible conditions', (t) => {
-  const uql = {$or: [{name: 'Leafar'}, {lastName: 'Huichops'}]};
-  const expected = [];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find no records with $or operator and impossible conditions', (assert) => {
+    const uql = {$or: [{name: 'Leafar'}, {lastName: 'Huichops'}]};
+    const expected = [];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find records with $and operator', (t) => {
-  const uql = {$and: [{name: 'Gibran'}, {lastName: 'Arias'}]};
-  const expected = [3];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find records with $and operator', (assert) => {
+    const uql = {$and: [{name: 'Gibran'}, {lastName: 'Arias'}]};
+    const expected = [3];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find no records with $and operator and impossible conditions', (t) => {
-  const uql = {$and: [{name: 'Gibran'}, {lastName: 'Argumedo'}]};
-  const expected = [];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find no records with $and operator and impossible conditions', (assert) => {
+    const uql = {$and: [{name: 'Gibran'}, {lastName: 'Argumedo'}]};
+    const expected = [];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find records with implicit and operator and a $lt operator in a single field', (t) => {
-  const uql = {tracked: true, createdAt: {$lt: new Date('2016-02-18T00:00:00.000Z')}};
-  const expected = [2, 3, 4];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find records with implicit and operator and a $lt operator in a single field', (assert) => {
+    const uql = {tracked: true, createdAt: {$lt: new Date('2016-02-18T00:00:00.000Z')}};
+    const expected = [2, 3, 4];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find value in a nested attribute', (t) => {
-  const uql = {'job.title': 'Programmer'};
-  const expected = [2, 4, 5, 6];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
-});
+  test('should find value in a nested attribute', (assert) => {
+    const uql = {'job.title': 'Programmer'};
+    const expected = [2, 4, 5, 6];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 
-test('should find value in a nested attribute', (t) => {
-  const uql = {'job.companyName': 'VoxFeed'};
-  const expected = [1, 2, 3, 4, 5];
-  const actual = filter(uql).map(p => p.id).sort();
-  t.equal(actual.join(), expected.join());
-  t.end();
+  test('should find value in a nested attribute', (assert) => {
+    const uql = {'job.companyName': 'VoxFeed'};
+    const expected = [1, 2, 3, 4, 5];
+    const actual = filter(uql).map(p => p.id).sort();
+    assert('equal', actual.join(), expected.join());
+  });
 });
--- test/drivers/postgres/transpiler.spec.js
+++ test/drivers/postgres/transpiler.spec.js
@@ -1,4 +1,4 @@
-const test = require('tape');
+const {suite} = require('suitape');
 
 const startDate = '2001-02-11T00:00:00.000Z';
 const endDate = '2001-02-13T00:00:00.000Z';
@@ -15,87 +15,80 @@ const schema = defineSchema('single_table', {
 });
 const transpile = PostgresTranspiler(schema);
 
-test('should create correct conditions with one condition', (t) => {
-  const uql = {name: 'Jon'};
-  const expected = 'WHERE name=\'Jon\'';
-  const actual = transpile(uql);
-  t.equal(actual, expected);
-  t.end();
-});
+suite('Postgres Transpiler', (test) => {
+  test('should create correct conditions with one condition', (assert) => {
+    const uql = {name: 'Jon'};
+    const expected = 'WHERE name=\'Jon\'';
+    const actual = transpile(uql);
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions with two conditions', (t) => {
-  const uql = {name: 'Jon', lastName: 'Doe'};
-  const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\'';
-  const actual = transpile(uql);
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should create correct conditions with two conditions', (assert) => {
+    const uql = {name: 'Jon', lastName: 'Doe'};
+    const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\'';
+    const actual = transpile(uql);
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions with three conditions', (t) => {
-  const uql = {name: 'Jon', lastName: 'Doe', age: 23};
-  const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\' AND age=23';
-  const actual = transpile(uql);
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should create correct conditions with three conditions', (assert) => {
+    const uql = {name: 'Jon', lastName: 'Doe', age: 23};
+    const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\' AND age=23';
+    const actual = transpile(uql);
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions with a date range condition', (t) => {
-  const uql = {
-    createdAt: {
-      $gte: new Date(startDate),
-      $lt: new Date(endDate)
-    }
-  };
-  const actual = transpile(uql);
-  const expected = `WHERE created_at >= \'${startDate}\' AND created_at < '${endDate}'`;
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should create correct conditions with a date range condition', (assert) => {
+    const uql = {
+      createdAt: {
+        $gte: new Date(startDate),
+        $lt: new Date(endDate)
+      }
+    };
+    const actual = transpile(uql);
+    const expected = `WHERE created_at >= \'${startDate}\' AND created_at < '${endDate}'`;
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions with three regular conditions and a date range condition', (t) => {
-  const regularConds = {name: 'Jon', lastName: 'Doe', age: 23};
-  const dateRange = {
-    createdAt: {
-      $gte: new Date(startDate),
-      $lt: new Date(endDate)
-    }
-  };
-  const uql = Object.assign({}, regularConds, dateRange);
-  const actual = transpile(uql);
-  const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\' AND age=23 AND ' +
-   `created_at >= \'${startDate}\' AND created_at < '${endDate}'`;
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should create correct conditions with three regular conditions and a date range condition', (assert) => {
+    const regularConds = {name: 'Jon', lastName: 'Doe', age: 23};
+    const dateRange = {
+      createdAt: {
+        $gte: new Date(startDate),
+        $lt: new Date(endDate)
+      }
+    };
+    const uql = Object.assign({}, regularConds, dateRange);
+    const actual = transpile(uql);
+    const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\' AND age=23 AND ' +
+     `created_at >= \'${startDate}\' AND created_at < '${endDate}'`;
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions with $or operator', (t) => {
-  const uql = {$or: [{name: 'Jon'}, {lastName: 'Doe'}]};
-  const expected = 'WHERE name=\'Jon\' OR last_name=\'Doe\'';
-  const actual = transpile(uql);
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should create correct conditions with $or operator', (assert) => {
+    const uql = {$or: [{name: 'Jon'}, {lastName: 'Doe'}]};
+    const expected = 'WHERE name=\'Jon\' OR last_name=\'Doe\'';
+    const actual = transpile(uql);
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions with explicit $and operator', (t) => {
-  const uql = {$and: [{name: 'Jon'}, {lastName: 'Doe'}]};
-  const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\'';
-  const actual = transpile(uql);
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should create correct conditions with explicit $and operator', (assert) => {
+    const uql = {$and: [{name: 'Jon'}, {lastName: 'Doe'}]};
+    const expected = 'WHERE name=\'Jon\' AND last_name=\'Doe\'';
+    const actual = transpile(uql);
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions with a single $lt operator', (t) => {
-  const uql = {tracked: true, createdAt: {$lt: new Date(startDate)}};
-  const expected = `WHERE tracked=true AND created_at < '${startDate}'`;
-  const actual = transpile(uql);
-  t.equal(actual, expected);
-  t.end();
-});
+  test('should create correct conditions with a single $lt operator', (assert) => {
+    const uql = {tracked: true, createdAt: {$lt: new Date(startDate)}};
+    const expected = `WHERE tracked=true AND created_at < '${startDate}'`;
+    const actual = transpile(uql);
+    assert('equal', actual, expected);
+  });
 
-test('should create correct conditions for single json inner query', (t) => {
-  const uql = {'job.title': 'Programmer'};
-  const expected = 'WHERE job->>\'title\'=\'Programmer\'';
-  const actual = transpile(uql);
-  t.equal(actual, expected);
-  t.end();
+  test('should create correct conditions for single json inner query', (assert) => {
+    const uql = {'job.title': 'Programmer'};
+    const expected = 'WHERE job->>\'title\'=\'Programmer\'';
+    const actual = transpile(uql);
+    assert('equal', actual, expected);
+  });
 });

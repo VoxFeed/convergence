cb5d80a1a326ff440afec9ac3e4a97ed821ab9aa
Jesus Agustin Pena Meza <jesusx21@gmail.com>
2016-07-25 12:20:37 -0500

    Add Find support on memory

--- test/run/drivers/memory/crud.spec.js
+++ test/run/drivers/memory/crud.spec.js
@@ -6,6 +6,7 @@ const store = {'persons': personsFixtures.map(clone)};
 const engine = memory(store);
 const model = require('test/test-helpers/build-single-table-schema')(engine);
 const isPlainObject = require('lodash/isPlainObject');
+const isArray = require('lodash/isArray');
 
 describe('Memory Crud', () => {
   const crud = Crud(engine, model);
@@ -19,11 +20,11 @@ describe('Memory Crud', () => {
 
     it('returns an error if unknown fields are sent', (done) => {
       crud.findOne({where: {unknown: 'field'}})
-        .then(record => {
-          expect(record).to.not.exist;
+        .then(() => done('unexpected data'))
+        .catch((error) => {
+          expect(error.name).to.be.equal('BAD_INPUT');
           done();
-        })
-        .catch(done);
+        });
     });
 
     it('should return a plain object', (done) => {
@@ -41,6 +42,47 @@ describe('Memory Crud', () => {
           expect(record).to.be.deep.equal(personsFixtures[1]);
         })
         .then(() => done())
+        .catch(done);
+    });
+  });
+
+  describe('Find', () => {
+    it('returns the correct records', (done) => {
+      const query = {where: {or: [{id: 1}, {id: 2}]}};
+      crud.find(query)
+        .then((persons) => {
+          const ids = persons.map(person => person.id);
+          expect(ids).to.be.deep.equal(ids);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('returns an empty array when not found records', (done) => {
+      const query = {where: {name: 'Jon', lastName: 'Snow'}};
+      crud.find(query)
+        .then((response) => expect(response).to.be.empty)
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('throws an error when unknown fields are sent', (done) => {
+      crud.find({where: {unknown: 'field'}})
+        .then(() => done('unexpected data'))
+        .catch((error) => {
+          expect(error.name).to.be.equal('BAD_INPUT');
+          done();
+        });
+    });
+
+    it('returns an array when filter has operators', (done) => {
+      const query = {where: {and: [{name: 'Jon'}, {lastName: 'Doe'}]}};
+      crud.find(query)
+        .then((response) => {
+          expect(isArray(response)).to.be.true;
+          expect(response).to.not.be.empty;
+          done();
+        })
         .catch(done);
     });
   });

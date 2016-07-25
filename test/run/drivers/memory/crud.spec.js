719835f657dea873b2047637f1b568abef849ae1
Jesus Agustin Pena Meza <jesusx21@gmail.com>
2016-07-25 15:43:19 -0500

    Add update and insert  support on memory

--- test/run/drivers/memory/crud.spec.js
+++ test/run/drivers/memory/crud.spec.js
@@ -1,15 +1,20 @@
-const clone = require('lodash/cloneDeep');
 const {memory} = require('lib/engines');
 const Crud = require('lib/drivers/memory/crud');
+const snakeobj = require('snakeobj');
 const personsFixtures = require('test/data/fixtures/persons');
-const store = {'persons': personsFixtures.map(clone)};
+const store = {'persons': personsFixtures.map(person => snakeobj(person))};
 const engine = memory(store);
 const model = require('test/test-helpers/build-single-table-schema')(engine);
 const isPlainObject = require('lodash/isPlainObject');
 const isArray = require('lodash/isArray');
+const unexpectedData = require('test/test-helpers/unexpected-data');
 
 describe('Memory Crud', () => {
-  const crud = Crud(engine, model);
+  let crud;
+
+  beforeEach(() => {
+    crud = Crud(engine, model);
+  });
 
   describe('Find One', () => {
     it('returns a promise', () => {
@@ -112,6 +117,165 @@ describe('Memory Crud', () => {
 
     it('should not return error if operators are sent', done => {
       crud.count({where: {and: [{name: 'Jon'}, {lastName: 'Doe'}]}})
+        .then(() => done())
+        .catch(done);
+    });
+  });
+
+  describe('Insert', () => {
+    it('should return promise', () => {
+      const actual = crud.insert({name: 'Jon'}).constructor.name;
+      const expected = 'Promise';
+      expect(actual).to.be.equal(expected);
+    });
+
+    it('should return error when trying to insert unkown field', done => {
+      crud.insert({unknown: 'Field'})
+        .then(unexpectedData)
+        .catch(err => expect(err.name).to.be.equal('BAD_INPUT'))
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('should create record', done => {
+      crud.insert({id: 999, name: 'Jon'})
+        .then(person => {
+          const expected = 'Jon';
+          const actual = person.name;
+          expect(actual).to.be.equal(expected);
+          return person;
+        })
+        .then(person => crud.findOne({where: {id: person.id}}))
+        .then(person => {
+          const expected = 'Jon';
+          const actual = person.name;
+          expect(actual).to.be.equal(expected);
+        })
+        .then(() => done())
+        .catch(done);
+    });
+  });
+
+  describe('Update', () => {
+    it('should update single existing record', done => {
+      const query = {where: {id: 1}};
+      const data = {lastName: 'Not Doe'};
+      const expectUpdate = person => {
+        const expected = 'Not Doe';
+        const actual = person.lastName;
+        expect(actual).to.be.equal(expected);
+        return person;
+      };
+
+      crud.update(query, data)
+        .then(expectUpdate)
+        .then(person => crud.findOne({where: {id: person.id}}))
+        .then(expectUpdate)
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('should return error when trying to update with unkown field in data', done => {
+      const query = {where: {name: 'Jon'}};
+      const data = {unknown: 'Field'};
+      crud.update(query, data)
+        .then(() => done('unexpected data'))
+        .catch(err => {
+          expect(err.name).to.be.equal('BAD_INPUT');
+          done();
+        });
+    });
+
+    it('should return error when trying to update with unkown field in query', done => {
+      const query = {where: {unknown: 'Field'}};
+      const data = {name: 'Jon'};
+      crud.update(query, data)
+        .then(() => done('unexpected data'))
+        .catch(err => {
+          expect(err.name).to.be.equal('BAD_INPUT');
+          done();
+        });
+    });
+
+    it('should return error if keyword where is missing', done => {
+      const query = {name: 'Jon'};
+      const data = {lastName: 'doe'};
+      crud.update(query, data)
+        .then(() => done('unexpected data'))
+        .catch(err => {
+          expect(err.name).to.be.equal('BAD_INPUT');
+          done();
+        });
+    });
+
+    it('should update property in json field', done => {
+      const query = {where: {id: 1}};
+      const data = {'job.companyName': 'new value'};
+      const expectUpdate = person => {
+        expect(person.job).to.have.property('companyName', 'new value');
+        return person;
+      };
+
+      crud.update(query, data)
+        .then(expectUpdate)
+        .then(person => crud.findOne({where: {id: person.id}}))
+        .then(expectUpdate)
+        .then(() => done())
+        .catch(done);
+    });
+  });
+
+  describe('Remove', () => {
+    it('should remove all records if no query is sent', done => {
+      const query = {where: {}};
+
+      crud.remove(query)
+        .then(persons => expect(persons.length).to.be.equal(6))
+        .then(() => crud.find(query))
+        .then(persons => expect(persons.length).to.be.equal(0))
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('should remove correct record with one field', done => {
+      const query = {where: {name: 'Jon'}};
+      crud.remove(query)
+        .then(persons => {
+          expect(persons.length).to.be.equal(1);
+          const person = persons.pop();
+          expect(person.name).to.be.equal('Jon');
+          return person;
+        })
+        .then(person => crud.findOne({where: {id: person.id}}))
+        .then(person => expect(person).not.to.exist)
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('should remove correct records with or operator', done => {
+      const query = {where: {or: [{name: 'Jon'}, {lastName: 'Arias'}]}};
+      crud.remove(query)
+        .then(persons => {
+          expect(persons.length).to.be.equal(2);
+          expect(persons.map(p => p.id).sort().join()).to.be.equal([1, 3].join());
+          return persons;
+        })
+        .then(() => crud.find(query))
+        .then(persons => expect(persons.length).to.be.equal(0))
+        .then(() => done())
+        .catch(done);
+    });
+
+    it('should create correct records for single json inner query', done => {
+      const query = {where: {'job.title': 'Programmer'}};
+      crud.remove(query)
+        .then(persons => {
+          expect(persons.length).to.be.equal(4);
+          expect(persons.map(p => p.id).sort().join()).to.be.equal([2, 4, 5, 6].join());
+          return persons;
+        })
+        .then(() => crud.find(query))
+        .then(persons => expect(persons.length).to.be.equal(0))
         .then(() => done())
         .catch(done);
     });
